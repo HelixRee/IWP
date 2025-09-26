8593c9f57ff48afde68535a681bbc2476b2aad31
HelixRee <68953458+HelixRee@users.noreply.github.com>
2025-09-27 00:01:44 +0800

    Feature: Overhauled head animator

--- Assets/Characters/BaseRalphAnimator.cs
+++ Assets/Characters/BaseRalphAnimator.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public abstract class BaseRalphAnimator : MonoBehaviour
 {
+    [HideInInspector]
     public LayerMask GroundLayers;
     public abstract void ManualInit();
     public abstract void ManualUpdate();
--- Assets/Characters/RalphHeadAnimator.cs
+++ Assets/Characters/RalphHeadAnimator.cs
@@ -1,47 +1,43 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class RalphHeadAnimator : BaseRalphAnimator
 {
-    public Transform HeadProxy;
-    [Range(0f, 1f)]
-    public float Weight = 1f;
+    [Range(0f, 1f)] public float Weight = 1f;
+    public Transform HeadTarget;
 
-    [Range(0f, 10f)] public float Frequency = 3f;
-    [Range(0f, 1f)] public float Damping = 0.5f;
-    [Range(0f, 10f)] public float Readiness = 2f;
+    public List<BaseRalphAnimator> ChildAnimations = new();
 
-    private SODAngle _smoothedAngle;
-    private float _nonSmoothedAngle;
+    private float _angleOffset = 0f;
     private Vector3 _initialAngles;
 
     public override void ManualInit()
     {
-        _smoothedAngle = new SODAngle(HeadProxy.eulerAngles.y, 3, 0.5f, 2);
-        _nonSmoothedAngle = HeadProxy.eulerAngles.y;
+        ChildAnimations.ForEach(anim => anim.ManualInit());
         _initialAngles = transform.localEulerAngles;
     }
 
     public override void ManualUpdate()
     {
-        _nonSmoothedAngle = HeadProxy.eulerAngles.y;
-        if (_nonSmoothedAngle < -180f) // -359f
-            _nonSmoothedAngle = _nonSmoothedAngle + 360f;
-        if (_nonSmoothedAngle > 180f) // 359f
-            _nonSmoothedAngle = _nonSmoothedAngle - 360f;
+        ChildAnimations.ForEach(anim => anim.ManualUpdate());
 
-
-        float smoothedAngle = _smoothedAngle.Update(Time.deltaTime, _nonSmoothedAngle);
-
-        float disp = (smoothedAngle - _nonSmoothedAngle);
-        if (disp < -180f) // -359f
-            disp = disp + 360f;
-        if (disp > 180f) // 359f
-            disp = disp - 360f;
-
-        //Debug.Log("Smoothed: " + smoothedAngle + ", NonSmoothed: " + _nonSmoothedAngle + ", Disp: " + disp);
+        transform.localEulerAngles = _initialAngles;
+        Vector3 offset = HeadTarget.position - transform.position;
+        float x = Vector3.Dot(offset, -transform.up);
+        float y = Vector3.Dot(offset, -transform.right);
+        Vector2 offset2D = new Vector2(x, y);
+        _angleOffset = Vector2.SignedAngle(offset2D, Vector2.up);
 
         Vector3 angles = _initialAngles;
-        angles.z += disp * Weight;
+        angles.z += _angleOffset * Weight;
         transform.localEulerAngles = angles;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.Lerp(Color.green, Color.red, Mathf.Abs(_angleOffset) / 10f);
+        Gizmos.DrawLine(transform.position, HeadTarget.position);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawRay(transform.position, -transform.right * (HeadTarget.position - transform.position).magnitude);
     }
 }
