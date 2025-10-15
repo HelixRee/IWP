2a70ff4f41e854d076d21bd155c5919ab842fb7a
HelixRee <68953458+HelixRee@users.noreply.github.com>
2025-10-15 19:23:30 +0800

    Actually fixed the antennae this time

--- Assets/Characters/Ralph 1.0/Scripts/Animations/RalphAntennaLookAt.cs
+++ Assets/Characters/Ralph 1.0/Scripts/Animations/RalphAntennaLookAt.cs
@@ -15,8 +15,35 @@ public class RalphAntennaLookAt : BaseRalphAnimator
 
     public override void ManualUpdate()
     {
-        transform.LookAt(Source, -Vector3.right);
-        transform.Rotate(Vector3.right, 90);
+        //transform.up = Source.position - transform.position;
+        //transform.Rotate(Vector3.up, 90);
+        //transform.LookAt(Source, -transform.parent.up);
+        //transform.Rotate(Vector3.right, 90);
+
+        //transform.LookAt(Source, -Vector3.right);
+        //transform.Rotate(Vector3.right, 90);
+        //Vector3 angles = transform.localEulerAngles;
+        //angles.y = _initialRotation.y;
+        //transform.localEulerAngles = angles;
+        //transform.Rotate(Vector3.up, Vector3.Dot(transform.parent.eulerAngles, transform.parent.up) - 90);
+
+        Vector3 disp = Source.position - transform.position;
+        float angleZ, angleX;
+        {
+            float x = Vector3.Dot(disp, -transform.parent.right);
+            float y = Vector3.Dot(disp, transform.parent.up);
+            angleZ = Vector2.SignedAngle(new Vector2(x, y), Vector2.up);
+        }
+        {
+            float x = Vector3.Dot(disp, transform.parent.forward);
+            float y = Vector3.Dot(disp, transform.parent.up);
+            angleX = Vector2.SignedAngle(new Vector2(x, y), Vector2.up);
+        }
+
+        Vector3 angle = _initialRotation.eulerAngles;
+        angle.x = angleX;
+        angle.z = angleZ;
+        transform.localEulerAngles = angle;
 
         transform.localRotation = Quaternion.Slerp(_initialRotation, transform.localRotation, Weight);
     }
@@ -25,5 +52,6 @@ public class RalphAntennaLookAt : BaseRalphAnimator
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawLine(transform.position, Source.position);
+        Gizmos.DrawRay(transform.position, transform.right * 0.01f);
     }
 }
