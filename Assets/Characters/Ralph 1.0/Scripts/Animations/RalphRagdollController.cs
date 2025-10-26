32b65c40fa5cb454ccbcf22a976cbfac62ba3c72
HelixRee <68953458+HelixRee@users.noreply.github.com>
2025-10-26 23:52:11 +0800

    Added ragdoll integration

--- Assets/Characters/Ralph 1.0/Scripts/Animations/RalphRagdollController.cs
+++ Assets/Characters/Ralph 1.0/Scripts/Animations/RalphRagdollController.cs
@@ -7,11 +7,23 @@ using UnityEditor;
 
 public class RalphRagdollController : MonoBehaviour
 {
+    [Header("Launch Parameters")]
+    [SerializeField] private Rigidbody _mainBody;
+    [SerializeField] private CharacterController _characterController;
+    [SerializeField] private float _launchPower = 10f;
+    private Vector3 _launchVelocity = Vector3.zero;
+    private Vector3 _prevPos = Vector3.zero;
+
+    [Space(10)]
     [SerializeField] private List<Rigidbody> _rigidbodies = new();
     [SerializeField] private List<Collider> _colliders = new();
 
     [SerializeField] private UnityEvent onBecomeRagdoll;
 
+    private void Start()
+    {
+        _prevPos = _characterController.center;
+    }
     public void StartRagdoll()
     {
         foreach (Rigidbody rb in _rigidbodies)
@@ -20,7 +32,14 @@ public class RalphRagdollController : MonoBehaviour
         foreach (Collider collider in _colliders)
             collider.enabled = true;
 
+        _mainBody.AddForce(_characterController.velocity * _launchPower, ForceMode.Impulse);
+
         onBecomeRagdoll.Invoke();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+            StartRagdoll();
     }
 }
 
